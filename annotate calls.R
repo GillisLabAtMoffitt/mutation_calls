be5b221469b3b16b0745e6501a0f6e7813d27e00
Christelle Leitzinger <55062754+chrisleitzinger@users.noreply.github.com>
2021-05-17 14:35:09 -0400

    Create filterd calls file

--- annotate calls.R
+++ annotate calls.R
@@ -15,10 +15,19 @@ extra_annotations <- extra_annotations %>%
   mutate(CHROM = str_match(Variant, "chr(.*?):")[,2]) %>% 
   mutate(POS = as.numeric(str_match(Variant, ":([[:digit:]]*)")[,2])) %>% 
   mutate(REF = str_match(Variant, ":[[:digit:]]*(.*)>")[,2]) %>% 
-  mutate(ALT = str_match(Variant, ">(.*)")[,2]) %>% 
-  rename(VAF_annot = "VAF")
+  mutate(ALT = str_match(Variant, ">(.*)")[,2])
 
 # Join with non_annotated_calls
 
-non_annotated_calls <- left_join(non_annotated_calls, extra_annotations, by = c("patient_id" = "Sample", "CHROM", "POS", "REF", "ALT"))
+non_annotated_calls <- left_join(non_annotated_calls, extra_annotations, by = c("patient_id" = "Sample", "CHROM", "POS", "REF", "ALT", "VAF", "DEPTH" = "Depth"))
+non_annotated_calls <- non_annotated_calls %>% 
+  filter(!(VAF < 0.02 |
+           # DEPTH < 50  |
+           # (Left_reads + Right_reads) < 10 |
+           (LOCATION == "intronic" | FUNCTION == "synonymous_SNV") |
+           (dbSNP == "PRESENT" & VAF >= 0.450 & VAF <= 0.550) |
+           # (dbSNP == "PRESENT" & VAF > 0.95) |
+           COSMIC.y == "NOT_CONFIRMED_SOMATIC")
+         )
 
+write_csv(non_annotated_calls, paste0(path, "/05172021/Annotation Filtered Calls " , format(Sys.time(),"%m%d%Y"), ".csv"))
